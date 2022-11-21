856f8633d932a551aa7f9e3cd99bbe2cb8ac85ad
TraceofLight <rickyjun45@gmail.com>
2022-11-21 17:00:30 +0900

    Feat: Make Navbar
    새로운 네비게이션바 구현

---
+++ final-pjt-front/src/views/FeaturedView.vue
@@ -0,0 +1,16 @@
+<template>
+  <LoginPage/>
+</template>
+
+<script>
+
+  import LoginPage from '@/components/LoginPage.vue'
+
+  export default {
+    name: 'FeaturedView',
+    components: {
+        LoginPage
+    },
+  }
+
+</script>
