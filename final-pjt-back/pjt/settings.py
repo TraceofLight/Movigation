f24a2ef5ca86c586431ccc8ba6d6a081eaa00de8
TraceofLight <rickyjun45@gmail.com>
2022-11-24 13:49:24 +0900

    Chore: ETC
    사용하지 않는 모듈 처리

--- final-pjt-back/pjt/settings.py
+++ final-pjt-back/pjt/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
     # Authentication
     'rest_framework',
     'rest_framework.authtoken',
-    'rest_framework_simplejwt.token_blacklist',
+    # 'rest_framework_simplejwt.token_blacklist',
     'dj_rest_auth',
 
     # Registration
